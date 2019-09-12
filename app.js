9f7aad07b00a83e4fe436a163f6cff186afedd73
bassiebangura <devbassiebangura@gmail.com>
2019-09-11 22:12:44 -0400

    Establish connection to database.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require('./db');
 
 (async () => {
   try {
+    await db.sequelize.authenticate();
     console.log("Connection to the database successful!");
   } catch (error) {
     console.error("Error connecting to the database: ", error);
